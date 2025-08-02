78c6b425fa30452a66aa6db0269f7f3397a77f70
emgixiii <emgi@live.in>
2025-08-02 13:15:09 +0530

    fix(todo-comments): use snacks picker

--- lua/astrocommunity/editing-support/todo-comments-nvim/init.lua
+++ lua/astrocommunity/editing-support/todo-comments-nvim/init.lua
@@ -1,6 +1,28 @@
+---@type LazySpec
 return {
   "folke/todo-comments.nvim",
   opts = {},
+  dependencies = { "folke/snacks.nvim" },
+  specs = {
+    {
+      "AstroNvim/astrocore",
+      opts = {
+        mappings = {
+          n = {
+            ["<Leader>fT"] = {
+              function()
+                if not package.loaded["todo-comments"] then -- make sure to load todo-comments
+                  require("lazy").load { plugins = { "todo-comments.nvim" } }
+                end
+                require("snacks").picker.todo_comments()
+              end,
+              desc = "Todo Comments",
+            },
+          },
+        },
+      },
+    },
+  },
   event = "User AstroFile",
   cmd = { "TodoTrouble", "TodoTelescope", "TodoLocList", "TodoQuickFix" },
 }
