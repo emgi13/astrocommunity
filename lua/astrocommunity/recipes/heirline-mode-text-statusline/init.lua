7d7814ef4de883c997a3afad85d6ccb052a9c2bd
emgixiii <emgi@live.in>
2025-08-04 21:48:46 +0530

    fix(heirline): mode and clock

--- lua/astrocommunity/recipes/heirline-mode-text-statusline/init.lua
+++ lua/astrocommunity/recipes/heirline-mode-text-statusline/init.lua
@@ -3,6 +3,7 @@ return {
   opts = function(_, opts)
     local status = require "astroui.status"
 
-    opts.statusline[1] = status.component.mode { mode_text = { padding = { left = 1, right = 1 } } } -- add the mode text
+    opts.statusline[1] =
+      status.component.mode { mode_text = { padding = { left = 1, right = 1 } }, hl = { fg = "black", bold = true } } -- add the mode text
   end,
 }
