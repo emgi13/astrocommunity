f41656fe49f93da1a7780c1e9de1c95e50206ee2
emgixiii <emgi@live.in>
2025-08-20 13:44:13 +0530

    fix: bufferline catppuccin colorscheme

--- lua/astrocommunity/colorscheme/catppuccin/init.lua
+++ lua/astrocommunity/colorscheme/catppuccin/init.lua
@@ -1,26 +1,35 @@
+---@type LazySpec
 return {
   "catppuccin/nvim",
   name = "catppuccin",
   lazy = true,
-  ---@type CatppuccinOptions
-  ---@diagnostic disable: missing-fields
-  opts = {
-    auto_integrations = true,
-    integrations = {
-      colorful_winsep = { color = "lavender" },
-      snacks = {
-        indent_scope_color = "lavender",
-      },
-    },
-    highlight_overrides = {
-      all = {
-        FloatBorder = {
-          fg = "#585b70",
-          bg = "NONE",
+  config = function(opts)
+    ---@type CatppuccinOptions
+    ---@diagnostic disable missing-fields
+    local def_opts = {
+      auto_integrations = true,
+      transparent_background = true,
+      integrations = {
+        colorful_winsep = { color = "lavender" },
+        snacks = {
+          indent_scope_color = "lavender",
         },
       },
-    },
-  },
+      highlight_overrides = {
+        all = {
+          FloatBorder = {
+            fg = "#585b70",
+            bg = "NONE",
+          },
+        },
+      },
+    }
+
+    opts = require("astrocore").extend_tbl(def_opts, opts)
+    local catp = require "catppuccin"
+    catp.setup(opts)
+    catp.options.integrations.bufferline = nil
+  end,
   specs = {
     {
       "akinsho/bufferline.nvim",
